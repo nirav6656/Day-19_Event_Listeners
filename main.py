aa7108bbbdfd105d2c4baf796fd5b80c2ab74dc7
niravpatel6656 <npatel8705@conestogac.on.ca>
2024-03-22 23:52:04 -0400

    Complete turtle race game

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from turtle import Turtle, Screen
-
+import random
 # Made object named Sheru (my dog name)
 # sheru = Turtle()
 screen = Screen()
@@ -39,16 +39,41 @@ screen = Screen()
 
 # -----------------------Turtle Race----------------------------
 
-screen.setup(width=500,height=400)
-# user_bet = screen.textinput(title="Make your bet",prompt="Which tutle win the race ? Enter a Color: ")
-colors = ["red","green","yellow","black"]
-start_y = -50
-for co in colors:
-    co = Turtle("turtle")
-    co.color("co")
-    co.penup()
-    co.goto(x=-250,y=start_y)
-    start_y+=50
+
+
+is_race_on = False
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["green","red", "orange", "yellow", "purple", "blue"]
+y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
+
+#Create 6 turtles
+# for turtle_index in range(0, 6):
+#     sheru = Turtle(shape="turtle")
+#     sheru.penup()
+#     sheru.color(colors[turtle_index])
+#     sheru.goto(x=-230, y=y_positions[turtle_index])
+#     all_turtles.append(sheru)
+#
+# if user_bet:
+#     is_race_on = True
+#
+# while is_race_on:
+#     for turtle in all_turtles:
+#         #230 is 250 - half the width of the turtle.
+#         if turtle.xcor() > 230:
+#             is_race_on = False
+#             winning_color = turtle.pencolor()
+#             if winning_color == user_bet:
+#                 print(f"You've won! The {winning_color} turtle is the winner!")
+#             else:
+#                 print(f"You've lost! The {winning_color} turtle is the winner!")
+#
+#         #Make each turtle move a random amount.
+#         rand_distance = random.randint(0, 10)
+#         turtle.forward(rand_distance)
 
 
 
